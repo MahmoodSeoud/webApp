5c2cd31f51bc5ccde33b82c681f77ad147247609
mahmood seoud <Mahmoodseoud@gmail.com>
2022-03-21 22:41:48 +0100

    Second Push

---
+++ webMyFirstApp/webApp/Controllers/FoodOrders.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webApp.Controllers
+{
+    public class FoodOrders : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Users()
+        {
+            return View("Users");
+        }
+
+        public IActionResult Toppings(string name, int numberOfTopping)
+        {
+            ViewBag.Name = name;    
+            ViewBag.NumberOfToppings = numberOfTopping; 
+            return View();
+        }
+    }
+}
